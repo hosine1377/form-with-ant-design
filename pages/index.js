def86d81ce139efd0a7f0f09a3829a4575e3dccb
masome hosine <hosine.masome@gmail.com>
2023-12-26 00:00:49 +0330

    change style and import style file in global style file and add footer in index page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import { Flex } from 'antd'
 import Forms from '@/components/Form/Forms'
 import Banner from '@/components/Banner/Banner'
+import Footer from '@/components/Footer/Footer'
 export default function Home() {
   return (
     <div>
       <Banner />
       <Forms />
+      <Footer />
     </div>
   )
 }
